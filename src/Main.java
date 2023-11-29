bc5756a0b7f9cfd4ef4f5f4af157fe69f8a86baf
christian.romero24 <cr882294@gmail.com>
2023-11-29 11:20:25 -0600

    added a statement added github added csa is cool

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Dog");
         System.out.println("Github");
+        System.out.println("CSA is cool");
     }
 }
